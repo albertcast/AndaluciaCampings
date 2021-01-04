a90a7bfdf728bc7ee574c545ee6a91b76ac5d0a6
Francisco Carlos Ordóñez Córdoba <48439703+1marshmallow1@users.noreply.github.com>
2021-01-04 17:17:49 +0100

    Brujula

--- AndaluciaCampings/app/src/main/java/com/example/andaluciacampings/InicioAplicacion.java
+++ AndaluciaCampings/app/src/main/java/com/example/andaluciacampings/InicioAplicacion.java
@@ -3,6 +3,15 @@ package com.example.andaluciacampings;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -12,13 +21,31 @@ import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class InicioAplicacion extends AppCompatActivity {
+public class InicioAplicacion extends AppCompatActivity implements SensorEventListener{
 
     Button boton_ubicacion, boton_perfil, boton_logout, boton_ranking;
     String username;
     BottomNavigationView bottomNav;
 
     private boolean locationPermission = false;
+
+    //Brujula
+    private TextView brujulaTextView;
+    private ImageView brujulaImageView;
+
+    private SensorManager sensorManager;
+    private Sensor accelerometerSensor, magnetometerSensor;
+
+    private float[] lastAccelerometer = new float[3];
+    private float[] lastMagnetometer = new float[3];
+    private float[] rotationMatrix = new float[9];
+    private float[] orientation = new float[3];
+
+    boolean isLastAccelerometerArrayCopied = false;
+    boolean isLastMagnetometerArrayCopied = false;
+
+    long lastUpdatedTime = 0;
+    float currentDegree = 0f;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,10 +56,6 @@ public class InicioAplicacion extends AppCompatActivity {
             Toast.makeText(this, getIntent().getStringExtra("perfil_guardar"),Toast.LENGTH_LONG).show();
         }
 
-        boton_ubicacion = (Button) findViewById(R.id.button_maps);
-        boton_perfil = (Button) findViewById(R.id.button_perfil_inicio);
-        boton_logout = (Button) findViewById(R.id.button_logout);
-        boton_ranking = (Button) findViewById(R.id.button_ranking_inicio);
         username = getIntent().getStringExtra("usuario");
 
         boton_ubicacion.setText(R.string.boton_location);
@@ -40,6 +63,14 @@ public class InicioAplicacion extends AppCompatActivity {
 
         bottomNav = (BottomNavigationView) findViewById(R.id.bottomNavigation);
         bottomNav.setOnNavigationItemSelectedListener(this::onNavigationItemSelected);
+
+        //Brujula
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        brujulaTextView = findViewById(R.id.brujulaTextView);
+        brujulaImageView =  findViewById(R.id.brujulaImageView);
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        magnetometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
     }
 
     public void Perfil(){
@@ -97,5 +128,59 @@ public class InicioAplicacion extends AppCompatActivity {
             finish();
         }, 300);
         return true;
+    }
+
+    //Brujula
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if(event.sensor == accelerometerSensor){
+            System.arraycopy(event.values, 0, lastAccelerometer, 0, event.values.length);
+            isLastAccelerometerArrayCopied = true;
+        } else if(event.sensor == magnetometerSensor){
+            System.arraycopy(event.values, 0, lastMagnetometer, 0, event.values.length);
+            isLastMagnetometerArrayCopied = true;
+        }
+
+        if(isLastAccelerometerArrayCopied && isLastMagnetometerArrayCopied && System.currentTimeMillis() - lastUpdatedTime>250){
+            SensorManager.getRotationMatrix(rotationMatrix, null, lastAccelerometer, lastMagnetometer);
+            SensorManager.getOrientation(rotationMatrix, orientation);
+
+            float azimuthInRadians = orientation[0];
+            float azimuthInDegree = (float) Math.toDegrees(azimuthInRadians);
+
+            RotateAnimation rotateAnimation =
+                    new RotateAnimation(currentDegree, -azimuthInDegree,
+                            Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+            rotateAnimation.setDuration(250);
+            rotateAnimation.setFillAfter(true);
+            brujulaImageView.startAnimation(rotateAnimation);
+
+            currentDegree = -azimuthInDegree;
+            lastUpdatedTime = System.currentTimeMillis();
+
+            int x = (int) azimuthInDegree;
+            brujulaTextView.setText(x + "°");
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        sensorManager.registerListener(this, accelerometerSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, magnetometerSensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        sensorManager.unregisterListener(this, accelerometerSensor);
+        sensorManager.unregisterListener(this, magnetometerSensor);
     }
 }
